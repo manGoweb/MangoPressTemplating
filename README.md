d5fcf2c23098f76d95adca9af9dfe1e91a506866
Jirka Vebr <jirkavebr@gmail.com>
2016-06-12 14:34:00 +0200

    Added flash message support

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Prepared templating workflow with filters and macros.
 ```php
 MangoPressTemplating::init();
 ```
+
+## Using flash messages
+In PHP:
+```php
+$param = flashSuccess('Something just went great!');
+wp_redirect(add_query_arg(FLASH_KEY, $param));
+```
+and then in latte much like in regular Nette templates.
---
+++ lib/flashes.php
@@ -0,0 +1,52 @@
+<?php
+
+define('FLASH_INFO', 'info');
+define('FLASH_SUCCESS', 'success');
+define('FLASH_WARNING', 'warning');
+define('FLASH_ERROR', 'danger');
+define('FLASH_KEY', '_fid');
+
+function getFlashParam() {
+	static $param;
+
+	if (!isset($param)) {
+		$param = isset($_GET[FLASH_KEY]) ? $_GET[FLASH_KEY] : Nette\Utils\Random::generate(4);
+	}
+	return $param;
+}
+
+function getFlashSession() {
+	global $App;
+
+	$param = getFlashParam();
+	return $App->getService('session')->getSection('Nette.Application.Flash/' . $param);
+}
+
+function flashMessage($message, $type = FLASH_INFO) {
+	$session = getFlashSession();
+
+	$messages = $session->flash;
+	$messages[] = (object) [
+		'message' => $message,
+		'type' => $type,
+	];
+	$session->flash = $messages;
+
+	return getFlashParam();
+}
+
+function flashInfo($message) {
+	return flashMessage($message, FLASH_INFO);
+}
+
+function flashSuccess($message) {
+	return flashMessage($message, FLASH_SUCCESS);
+}
+
+function flashWarning($message) {
+	return flashMessage($message, FLASH_WARNING);
+}
+
+function flashError($message) {
+	return flashMessage($message, FLASH_ERROR);
+}
