a849820e1c31cf899c2107d7f0435613e6fd4f75
Viliam Kopecký <vk@viliamkopecky.cz>
2017-09-21 10:26:10 +0200

    add missing second parameter for the_title filter

--- lib/filters.php
+++ lib/filters.php
@@ -33,7 +33,7 @@ MangoFilters::$set['wp_title'] = function($id) {
 	$post = lazy_post($id);
 	if(!$post) return $id;
 
-	return safe(apply_filters('the_title', $post->post_title));
+	return safe(apply_filters('the_title', $post->post_title, $post->ID));
 };
 
 MangoFilters::$set['wp_content'] = function($id) {
